a68358593aeb8c8f9b6d24d97f06653be4cdf734
Liam Self <liam@liamself.co.uk>
2020-11-26 23:42:02 +0000

    added Warp firmware from CW2

---
+++ build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/CMakeFiles/KsdkPlatformLib.dir/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/platform/system/src/clock/fsl_clock_manager_common.c.d
@@ -0,0 +1,32 @@
+CMakeFiles/KsdkPlatformLib.dir/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/platform/system/src/clock/fsl_clock_manager_common.c.obj: \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/platform/system/src/clock/fsl_clock_manager_common.c \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/fsl_device_registers.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cm0plus.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cmInstr.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cmFunc.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/startup/MKL03Z4/system_MKL03Z4.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_registers.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/fsl_bitaccess.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_features.h
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/fsl_device_registers.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cm0plus.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cmInstr.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cmFunc.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/startup/MKL03Z4/system_MKL03Z4.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_registers.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/fsl_bitaccess.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_features.h:
