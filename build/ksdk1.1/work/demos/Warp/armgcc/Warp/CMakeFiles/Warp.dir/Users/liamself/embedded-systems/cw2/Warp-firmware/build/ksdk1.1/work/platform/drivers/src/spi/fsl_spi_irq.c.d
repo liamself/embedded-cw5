3e1c5b5a526d69437d26ec290aa8ce590a77e40f
Liam Self <liam@liamself.co.uk>
2020-11-27 00:21:01 +0000

    re-added warp files from mac

---
+++ build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/platform/drivers/src/spi/fsl_spi_irq.c.d
@@ -0,0 +1,38 @@
+CMakeFiles/Warp.dir/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/platform/drivers/src/spi/fsl_spi_irq.c.obj: \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/platform/drivers/src/spi/fsl_spi_irq.c \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/platform/drivers/src/spi/fsl_spi_shared_function.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/fsl_device_registers.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cm0plus.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cmInstr.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cmFunc.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/startup/MKL03Z4/system_MKL03Z4.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_registers.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/fsl_bitaccess.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_features.h \
+ /Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/hal/inc/fsl_spi_hal.h
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/platform/drivers/src/spi/fsl_spi_shared_function.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/fsl_device_registers.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cm0plus.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cmInstr.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cmFunc.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/startup/MKL03Z4/system_MKL03Z4.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_registers.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/fsl_bitaccess.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_features.h:
+
+/Users/liamself/embedded-systems/cw2/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/hal/inc/fsl_spi_hal.h:
